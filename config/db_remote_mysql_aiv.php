e115b9303af6dfc60fa745c9953ea196bcd58190
Aleksandr Marchenko <av_marchenko@yahoo.com>
2017-08-22 11:05:29 +0300

    connect to remote aiv

---
+++ config/db_remote_mysql_aiv.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=aiv:3307;dbname=aiv',
+    'username' => 'kinematic',
+    'password' => 'marchuk',
+    'charset' => 'utf8',
+];
