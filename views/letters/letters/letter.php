86db918f269c1c7f17fcbcaaa696f35a7691c07e
Aleksandr Marchenko <av_marchenko@yahoo.com>
2018-11-16 09:52:45 +0200

    20181116

--- views/letters/letters/letter.php
+++ views/letters/letters/letter.php
@@ -2,15 +2,32 @@
 // require_once '@vendor\autoload.php';
 $phpWord = new \PhpOffice\PhpWord\PhpWord();
 
-$section = $phpWord->addSection();
-// Adding Text element to the Section having font styled by default...
-$section->addText(
-    '"Learn from yesterday, live for today, hope for tomorrow. '
-        . 'The important thing is not to stop questioning." '
-        . '(Albert Einstein)'
-);
+$phpWord->setDefaultFontName('Courier New');
+$phpWord->setDefaultFontSize(8);
 
-// Saving the document as OOXML file...
+$properties = $phpWord->getDocInfo();
+$properties->setCreator('AIV');
+$properties->setCompany("ПрАТ 'Київстар'");
+$properties->setTitle('Письмо на проход');
+$properties->setDescription('Письмо на проход');
+$properties->setCategory('Доступ');
+$properties->setLastModifiedBy('Александр Марченко');
+$properties->setCreated(mktime(0, 0, 0, 3, 12, 2014));
+$properties->setModified(mktime(0, 0, 0, 3, 14, 2014));
+$properties->setSubject('My subject');
+$properties->setKeywords('my, key, word');
+
+$sectionStyle = array(
+	'orientation' => 'landscape',
+	'marginTop' => \PhpOffice\PhpWord\Shared\Converter::cmToTwip(1),
+);
+$section = $phpWord->addSection($sectionStyle);
+$section->addText('"___" _________ 2018 р. № ___________');
+$section->addText('Директору
+ТОВ «ПРОПЕРТІ МЕНЕДЖМЕНТ СОЛЮШИНЗ ТУ»
+п. Фощану О.М.
+', array(), array());
+
 $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
 $objWriter->save('helloWorld.docx');
 
